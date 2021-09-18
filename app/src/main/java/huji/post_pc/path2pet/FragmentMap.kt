8a6b6792f2cd1fbbdc342fe809c71bb7990dbcdd
Nivi <nivi.shenker@mail.huji.ac.il>
2021-09-18 12:19:18 +0300

    add google maps search

--- app/src/main/java/huji/post_pc/path2pet/FragmentMap.kt
+++ app/src/main/java/huji/post_pc/path2pet/FragmentMap.kt
@@ -1,5 +1,6 @@
 package huji.post_pc.path2pet
-import android.app.FragmentTransaction;
+import android.animation.ValueAnimator
+import android.location.Address
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,34 +9,68 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+
+import com.google.android.gms.maps.model.LatLng
+
+import android.location.Geocoder
+import androidx.appcompat.widget.SearchView
+import java.lang.Exception
+
 
 class FragmentMap : Fragment() {
-
     private val onboardingViewModel: LostPetViewModel by activityViewModels()
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map, container, false)
 
         // find views
+        val searchView: SearchView = view.findViewById(R.id.idSearchView)
         val nextButton: Button = view.findViewById(R.id.next)
         val prevButton: Button = view.findViewById(R.id.previous)
 
         //google map
         val mapFragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
+            mMap = it
             it.setOnMapClickListener { coordinate->
-                it.addMarker(
+                it.clear()
+                it.addMarker(MarkerOptions().position(coordinate))
+          }
+        })
 
-                    MarkerOptions().position(coordinate)
-                )
+        // search view query
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(s: String): Boolean {
+                val searchInitLocation = searchView.query.toString()
+                var addressList: List<Address>? = null
+                if (searchInitLocation != null && searchInitLocation != "") {
+                    val geocoder = Geocoder(view.context)
+                    try {
+                        addressList = geocoder.getFromLocationName(searchInitLocation, 1)
+                    }
+                    catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                    val address: Address = addressList!![0]
+                    val latLng = LatLng(address.getLatitude(), address.getLongitude())
+                    mMap.addMarker(MarkerOptions().position(latLng).title(searchInitLocation))
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                }
+                return false
+            }
 
+            override fun onQueryTextChange(s: String): Boolean {
+                return false
             }
         })
+
 
         // next listener
         nextButton.setOnClickListener {
