aa464e051a03b0bc662bdb296bfaba5aef6122ab
Nivi <nivi.shenker@mail.huji.ac.il>
2021-09-18 19:25:00 +0300

    general map improvements

--- app/src/main/java/huji/post_pc/path2pet/FragmentMap.kt
+++ app/src/main/java/huji/post_pc/path2pet/FragmentMap.kt
@@ -1,4 +1,5 @@
 package huji.post_pc.path2pet
+import android.Manifest
 import android.animation.ValueAnimator
 import android.location.Address
 import android.os.Bundle
@@ -19,21 +20,53 @@ import com.google.android.gms.maps.model.LatLng
 
 import android.location.Geocoder
 import androidx.appcompat.widget.SearchView
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import java.lang.Exception
+import androidx.fragment.app.FragmentActivity
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+
+import com.google.android.libraries.places.api.Places
+
+import com.google.android.libraries.places.api.net.PlacesClient
+import java.util.*
+import androidx.core.content.ContextCompat.getSystemService
+
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
 
 
 class FragmentMap : Fragment() {
     private val onboardingViewModel: LostPetViewModel by activityViewModels()
     private lateinit var mMap: GoogleMap
+    private lateinit var myContext: Context
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var currentLocation: LatLng
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map, container, false)
+        myContext = view.context
 
         // find views
-        val searchView: SearchView = view.findViewById(R.id.idSearchView)
         val nextButton: Button = view.findViewById(R.id.next)
         val prevButton: Button = view.findViewById(R.id.previous)
+        val searchView: SearchView = view.findViewById(R.id.idSearchView)
+//        val autocompleteFragment = childFragmentManager.fragments[1] as AutocompleteSupportFragment
 
         //google map
         val mapFragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
@@ -54,22 +87,43 @@ class FragmentMap : Fragment() {
                     val geocoder = Geocoder(view.context)
                     try {
                         addressList = geocoder.getFromLocationName(searchInitLocation, 1)
+                        if (addressList.isEmpty()) {
+                            throw Exception()
+                        }
                     }
                     catch (e: Exception) {
                         e.printStackTrace()
+                        return false
                     }
                     val address: Address = addressList!![0]
                     val latLng = LatLng(address.getLatitude(), address.getLongitude())
                     mMap.addMarker(MarkerOptions().position(latLng).title(searchInitLocation))
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
                 }
-                return false
+                return true
             }
 
             override fun onQueryTextChange(s: String): Boolean {
+
                 return false
+
             }
         })
+
+//        // places suggestions
+//        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+//        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+//            override fun onPlaceSelected(place: Place) {
+//                val latLng = place.latLng
+//                if (latLng != null) {
+//                    mMap.addMarker(MarkerOptions().position(latLng).title(place.name))
+//                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+//                }
+//            }
+//            override fun onError(status: Status) {
+//                Log.i(TAG, "Google maps: places suggestions: An error occurred: $status")
+//            }
+//        })
 
 
         // next listener
@@ -81,7 +135,6 @@ class FragmentMap : Fragment() {
         prevButton.setOnClickListener {
             prevButtonOnClick(it)
         }
-
         return view
     }
 
--- app/src/main/java/huji/post_pc/path2pet/MainActivity.kt
+++ app/src/main/java/huji/post_pc/path2pet/MainActivity.kt
@@ -1,9 +1,13 @@
 package huji.post_pc.path2pet
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,17 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val lostPet = findViewById<Button>(R.id.lost_pet)
         val foundPet = findViewById<Button>(R.id.found_pet)
+
         // lost pet
         lostPet.setOnClickListener(){
             val intentLost = Intent(this, LostPetProcess::class.java)
             startActivity(intentLost)
         }
+
         // found pet
         foundPet.setOnClickListener(){
 //            val intentFound = Intent(this, lostPetProcess::class.java)
 //            startActivity(intentFound)
         }
-
-
     }
 }
