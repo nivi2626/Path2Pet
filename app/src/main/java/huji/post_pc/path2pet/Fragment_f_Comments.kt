3d25f471158a038415a591e47c6f4dd0d2bd58cf
oramar1256 <or.amar@mail.huji.ac.il>
2021-09-18 20:07:43 +0300

    added comments to sp

--- app/src/main/java/huji/post_pc/path2pet/Fragment_f_Comments.kt
+++ app/src/main/java/huji/post_pc/path2pet/Fragment_f_Comments.kt
@@ -1,15 +1,15 @@
 package huji.post_pc.path2pet
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
-import huji.post_pc.path2pet.LostPetViewModel
-import huji.post_pc.path2pet.R
 
 
 class Fragment_f_Comments : Fragment() {
@@ -17,16 +17,39 @@ class Fragment_f_Comments : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_comments, container, false)
+        val view = inflater.inflate(R.layout.fragment_f_comments, container, false)
+        val sp = this.activity?.getSharedPreferences("local_lost_db", Context.MODE_PRIVATE)
+        var userComments : String? = ""
 
         // find views
         val nextButton: Button = view.findViewById(R.id.next)
         val prevButton: Button = view.findViewById(R.id.previous)
-        // set UI
-        //TODO - if we already have the info - show it
+        val userCommentsText: EditText = view.findViewById(R.id.comments)
+
+        // get data from sp
+        if (sp != null) {
+            userComments = sp.getString("COMMENTS", "")
+        }
+
+        // set data by sp
+        if (userComments!=null)
+        {
+            if (userComments.isNotEmpty())
+            {
+                userCommentsText.setText(userComments)
+            }
+        }
 
         // next listener
         nextButton.setOnClickListener {
+            userComments = userCommentsText.text.toString()
+            if (sp != null) {
+                with(sp.edit())
+                {
+                    putString("COMMENTS", userComments)
+                    apply()
+                }
+            }
             nextButtonOnClick(it)
         }
 
@@ -34,6 +57,7 @@ class Fragment_f_Comments : Fragment() {
         prevButton.setOnClickListener {
             prevButtonOnClick(it)
         }
+
         return view
 
     }
