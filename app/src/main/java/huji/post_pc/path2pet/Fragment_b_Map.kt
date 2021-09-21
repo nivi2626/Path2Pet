19e03d82c7ab50d8c28108e4e6eb9fd4c19903ef
oramar1256 <or.amar@mail.huji.ac.il>
2021-09-21 21:14:02 +0300

    playing with maps - still losing

--- app/src/main/java/huji/post_pc/path2pet/Fragment_b_Map.kt
+++ app/src/main/java/huji/post_pc/path2pet/Fragment_b_Map.kt
@@ -26,14 +26,17 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
+import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.location.*
 
 
 class Fragment_b_Map : Fragment() {
@@ -41,17 +44,46 @@ class Fragment_b_Map : Fragment() {
     private lateinit var myContext: Context
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var currentLocation: LatLng
+    private lateinit var lostPetActivityInstance: LostPetProcess
+    private var fusedLocationProvider: FusedLocationProviderClient? = null
+    private val locationRequest: LocationRequest =  LocationRequest.create().apply {
+        interval = 30
+        fastestInterval = 10
+        priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        maxWaitTime= 60
+    }
+
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                //The last location in the list is the newest
+                val location = locationList.last()
+                Toast.makeText(
+                    myContext,
+                    "Got Location: " + location.toString(),
+                    Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_b_map, container, false)
         myContext = view.context
-        val lostPetActivityInstance: LostPetProcess? = activity as LostPetProcess?
+        lostPetActivityInstance = activity as LostPetProcess
 
         // find views
         val nextButton: Button = view.findViewById(R.id.next)
         val prevButton: Button = view.findViewById(R.id.previous)
-        val searchView: SearchView = view.findViewById(R.id.idSearchView)
+        var searchView: SearchView = view.findViewById(R.id.idSearchView)
+        val geocoder : Geocoder
+
+
+//        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(myContext)
+//        checkLocationPermission()
+
 
         // TODO - try to ask permissions
         if (ContextCompat.checkSelfPermission(myContext,
@@ -68,11 +100,23 @@ class Fragment_b_Map : Fragment() {
         }
         // TODO - end of try to ask permissions
 
+//
+//
         //google map
         val mapFragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
             // todo - make map go to your current location
             mMap = it
+
+            val point = CameraUpdateFactory.newLatLng(LatLng(32.109333, 34.855499))
+            val latLng = LatLng(32.109333, 34.855499)
+            val home = mMap.addMarker(MarkerOptions().position(latLng).title("Home"))
+            home.tag = -1
+            // moves camera to coordinates
+            mMap.moveCamera(point)
+            // animates camera to coordinates
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12f))
+
             it.setOnMapClickListener { coordinate->
                 it.clear()
                 it.addMarker(MarkerOptions().position(coordinate))
@@ -82,7 +126,7 @@ class Fragment_b_Map : Fragment() {
 
         //TODO - add location to SP (key = AppPath2Pet.SP_LOCATION)
 
-        // search view query
+//         search view query
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(s: String): Boolean {
                 val searchInitLocation = searchView.query.toString()
@@ -101,6 +145,7 @@ class Fragment_b_Map : Fragment() {
                     }
                     val address: Address = addressList!![0]
                     val latLng = LatLng(address.latitude, address.longitude)
+                    mMap.clear()
                     mMap.addMarker(MarkerOptions().position(latLng).title(searchInitLocation))
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
                 }
@@ -115,13 +160,13 @@ class Fragment_b_Map : Fragment() {
 
         // next listener
         nextButton.setOnClickListener {
-            lostPetActivityInstance!!.progressBar.incrementProgressBy(1)
+            lostPetActivityInstance.progressBar.incrementProgressBy(1)
             nextButtonOnClick(it)
         }
 
         // prev listener
         prevButton.setOnClickListener {
-            lostPetActivityInstance!!.onBackPressed()
+            lostPetActivityInstance.onBackPressed()
         }
 
 //        // todo - places suggestions
@@ -150,22 +195,137 @@ class Fragment_b_Map : Fragment() {
 
     // ask permissions code
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        when (requestCode) {
-            1 -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED) {
-                    if ((ContextCompat.checkSelfPermission(myContext,
-                            Manifest.permission.ACCESS_FINE_LOCATION) ===
-                                PackageManager.PERMISSION_GRANTED)) {
-                        Toast.makeText(myContext, "Permission Granted", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    Toast.makeText(myContext, "Permission Denied", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-        }
-    }
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+//                                            grantResults: IntArray) {
+//        when (requestCode) {
+//            1 -> {
+//                if (grantResults.isNotEmpty() && grantResults[0] ==
+//                    PackageManager.PERMISSION_GRANTED) {
+//                    if ((ContextCompat.checkSelfPermission(myContext,
+//                            Manifest.permission.ACCESS_FINE_LOCATION) ===
+//                                PackageManager.PERMISSION_GRANTED)) {
+//                        Toast.makeText(myContext, "Permission Granted", Toast.LENGTH_SHORT).show()
+//                    }
+//                } else {
+//                    Toast.makeText(myContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+//                }
+//                return
+//            }
+//        }
+//    }
+
+//    override fun onResume() {
+//        super.onResume()
+//        if (ContextCompat.checkSelfPermission(myContext, Manifest.permission.ACCESS_FINE_LOCATION)
+//            == PackageManager.PERMISSION_GRANTED) {
+//
+//            fusedLocationProvider?.requestLocationUpdates(
+//                locationRequest,
+//                locationCallback,
+//                Looper.getMainLooper()
+//            )
+//        }
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        if (ContextCompat.checkSelfPermission(myContext,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//            == PackageManager.PERMISSION_GRANTED) {
+//
+//            fusedLocationProvider?.removeLocationUpdates(locationCallback)
+//        }
+//    }
+//
+//    private fun checkLocationPermission() {
+//        if (ActivityCompat.checkSelfPermission(
+//                myContext,
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(
+//                    lostPetActivityInstance,
+//                    Manifest.permission.ACCESS_FINE_LOCATION
+//                )
+//            ) {
+//                // Show an explanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//                AlertDialog.Builder(myContext)
+//                    .setTitle("Location Permission Needed")
+//                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+//                    .setPositiveButton(
+//                        "OK"
+//                    ) { _, _ ->
+//                        //Prompt the user once explanation has been shown
+//                        requestLocationPermission()
+//                    }
+//                    .create()
+//                    .show()
+//            } else {
+//                // No explanation needed, we can request the permission.
+//                requestLocationPermission()
+//            }
+//        }
+//    }
+//
+//    private fun requestLocationPermission() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            ActivityCompat.requestPermissions(
+//                lostPetActivityInstance,
+//                arrayOf(
+//                    Manifest.permission.ACCESS_FINE_LOCATION,
+//                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+//                ),
+//                MY_PERMISSIONS_REQUEST_LOCATION
+//            )
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                lostPetActivityInstance,
+//                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                MY_PERMISSIONS_REQUEST_LOCATION
+//            )
+//        }
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        when (requestCode) {
+//            MY_PERMISSIONS_REQUEST_LOCATION -> {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                    // permission was granted, yay! Do the
+//                    // location-related task you need to do.
+//                    if (ContextCompat.checkSelfPermission(
+//                            myContext,
+//                            Manifest.permission.ACCESS_FINE_LOCATION
+//                        ) == PackageManager.PERMISSION_GRANTED
+//                    ) {
+//                        fusedLocationProvider?.requestLocationUpdates(
+//                            locationRequest,
+//                            locationCallback,
+//                            Looper.getMainLooper()
+//                        )
+//                    }
+//
+//                } else {
+//
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                    Toast.makeText(myContext, "permission denied", Toast.LENGTH_LONG).show()
+//                }
+//                return
+//            }
+//        }
+//    }
+//
+//    companion object {
+//        private const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+//    }
+
 }
