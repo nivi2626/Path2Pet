40a7b9d228f6202e91011dc293c4869f184f2477
oramar1256 <or.amar@mail.huji.ac.il>
2021-09-23 18:21:32 +0300

    navgraph for found

---
+++ app/src/main/java/huji/post_pc/path2pet/FoundProcess/Fragment_a_Camera.java
@@ -0,0 +1,25 @@
+package huji.post_pc.path2pet.FoundProcess;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import huji.post_pc.path2pet.R;
+
+
+public class Fragment_a_Camera extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.found_fragment_a_camera, container, false);
+
+        return view;
+    }
+}
