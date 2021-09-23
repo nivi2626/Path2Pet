a04f5711cbbb21164b43444dee39d2d229eb76d9
oramar1256 <or.amar@mail.huji.ac.il>
2021-09-23 23:10:29 +0300

    edited adapter

--- app/src/main/java/huji/post_pc/path2pet/MyLostPetsAdapter.kt
+++ app/src/main/java/huji/post_pc/path2pet/MyLostPetsAdapter.kt
@@ -1,6 +1,8 @@
 package huji.post_pc.path2pet
 
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.util.Log
@@ -8,11 +10,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.PopupWindow
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 
 class MyLostPetsAdapter : RecyclerView.Adapter<MyLostPetsViewHolder>() {
@@ -23,34 +24,92 @@ class MyLostPetsAdapter : RecyclerView.Adapter<MyLostPetsViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyLostPetsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.my_lost_pets_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.my_lost_pets_item, parent, false)
         return MyLostPetsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyLostPetsViewHolder, position: Int) {
         val curPet = petList[position]
+        val adapterContext: Context = holder.petType.context
+        val lostPetSp:SharedPreferences = adapterContext.getSharedPreferences("my_lost_pets", android.content.Context.MODE_PRIVATE)
+//        holder.itemId.text = curPet.getId()
         holder.petType.text = curPet.getPetType()
         holder.breed.text = curPet.getBreed()
         holder.city.text = "Jerusalem" // TODO - implement later
         holder.date.text = curPet.getLastSeenDate().toString()
 //        holder.image!!.text = curPet.getPetType() // TODO implement later
         holder.foundButton.setOnClickListener {
-            // TODO - add pop up - "are you sure you want to delete this entry?"
+            exitDialog(adapterContext, curPet, lostPetSp)
             // TODO - delete sp
-            // TODO - update FIREBASE
-            // TODO - remove log.d
-            Log.d("TAG", "found found found!")
+
         }
-//        holder.editButton.setOnClickListener {
-//            // TODO - implement, add Done button, change visibility, change other
-//            //  text views to edit text views
-//            Log.d("TAG", "edit edit edit!")
-//        }
     }
 
 
     override fun getItemCount(): Int {
         return petList.size
+    }
+
+    fun exitDialog(context: Context, pet: Pet, sp:SharedPreferences) {
+        val dialogBuilder = AlertDialog.Builder(context)
+        dialogBuilder.setView(View.inflate(context, R.layout.alert_dialog, null))
+
+        // set message of alert dialog
+        dialogBuilder.setMessage("Are you sure you wish to delete this entry?\npet's data will be lost")
+            // if the dialog is cancelable
+            .setCancelable(false)
+            // positive button text and action
+            .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
+
+                // delete from storage
+                if (pet.imageNum > 0)
+                {
+                    AppPath2Pet.getPetsDB().deleteFromStorage(pet.getId())
+                }
+
+                // delete data from fireStore
+                AppPath2Pet.getPetsDB().deleteFromFireStore(pet.getId())
+
+                // delete from sp
+                spUpdater(sp, pet.getId())
+
+                // hurray message
+                val toast = Toast.makeText(
+                    context,
+                    "Hurray!\nWe are glad to hear you found your pet!",
+                    Toast.LENGTH_LONG
+                )
+                toast.show()
+
+                notifyDataSetChanged()
+            })
+            // negative button text and action
+            .setNegativeButton("No", DialogInterface.OnClickListener { dialog, id ->
+                dialog.cancel()
+            })
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Cancel Report")
+        // show alert dialog
+        alert.show()
+    }
+
+    private fun spUpdater(sp: SharedPreferences, idToDelete: String) {
+        var newIDsList : String = ""
+        val userLostPetsIDs: Array<String> =
+            sp.getString(AppPath2Pet.SP_LOST_ID, "")!!.split(AppPath2Pet.SP_DELIMITER)
+                .toTypedArray()
+        for (id in userLostPetsIDs)
+        {
+            if (id != idToDelete)
+            {
+                newIDsList += id+AppPath2Pet.SP_DELIMITER
+            }
+        }
+        sp.edit().putString(AppPath2Pet.SP_LOST_ID, newIDsList).apply()
     }
 
 }
